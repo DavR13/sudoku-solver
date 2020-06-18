ce4cae45c4afe1cc885791424c60578854bcd45f
DavR13 <55868116+DavR13@users.noreply.github.com>
2020-06-18 14:08:39 -0700

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,54 +1,54 @@
-def print_brdard(brd):
+def print_board(board):
 
-    for i in range(len(brd)):
+    for i in range(len(board)):
         if i%3 == 0 and i != 0:
             print('- - - - - - - - - - - - - ')
 
-        for j in range(len(brd[0])):
+        for j in range(len(board[0])):
             if j%3 == 0 and j != 0:
                 print(' | ', end="")
 
             if j == 8:
-                print(brd[i][j])
+                print(board[i][j])
 
             else:
-                print(str(brd[i][j]) + " ", end='')
+                print(str(board[i][j]) + " ", end='')
 
-def find_empty(brd):
-    for i in range(len(brd)):
-        for j in range(len(brd[0])):
-            if brd[i][j] == 0:
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
                 return (i, j) #row, col
 
     return None
 
-def is_valid(brd, num, pos):
+def is_valid(board, num, pos):
 
     #check row
-    for i in range(len((brd[0]))):
-        if brd[pos[0]][i] == num and pos[1] != i:
+    for i in range(len((board[0]))):
+        if board[pos[0]][i] == num and pos[1] != i:
             return False
 
     #check column
-    for i in range(len(brd)):
-        if brd[i][pos[1]] == num and pos[0] != i:
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
             return False
 
     #check 3x3 grid
-    brdx_x = pos[1] // 3
-    brdx_y = pos[0] // 3
+    boardx_x = pos[1] // 3
+    boardx_y = pos[0] // 3
 
-    for i in range(brdx_y*3, brdx_y*3+3):
-        for j in range(brdx_x*3, brdx_x*3+3):
-            if brd[i][j] == num and (i,j) != pos:
+    for i in range(boardx_y*3, boardx_y*3+3):
+        for j in range(boardx_x*3, boardx_x*3+3):
+            if board[i][j] == num and (i,j) != pos:
                 return False
-
+            
     return True
 
 
-def solve(brd):
+def solve(board):
 
-    find = find_empty(brd)
+    find = find_empty(board)
 
     if not find:
         return True
@@ -56,14 +56,13 @@ def solve(brd):
         row, col = find
 
     for i in range(1,10):
-        if is_valid(brd, i, (row, col)):
-            brd[row][col] = i
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
 
-            if solve(brd):
+            if solve(board):
                 return True
 
-            brd[row][col] = 0
-
+            board[row][col] = 0
 
     return False
 
@@ -79,9 +78,9 @@ test = [
        [6, 5, 0, 0, 9, 0, 4, 0, 0]
        ]
 
-print_brdard(test)
+print_board(test)
 solve(test)
 print("__________________________________")
-print_brdard(test)
+print_board(test)
 
 
